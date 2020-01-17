c3b86536272a61453b275ad0316e7814b6aa4461
meli castorena <mcastorena0316@gmail.com>
2020-01-17 12:05:54 -0600

    Add Controller Users and methods for signin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_165750) do
+ActiveRecord::Schema.define(version: 2020_01_17_172627) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_165750) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
